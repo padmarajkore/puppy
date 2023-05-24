101e31c0fdf6c0771abfc1f7b227a11c259d8d82
TEST <padmaraj.com@gmail.com>
2023-05-24 23:55:35 +0530

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/padmarajkore/puppy
 
 go 1.13
+
+require github.com/padmarajkore/dog v0.0.0-20230524182154-a2f06485d4b4 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,23 @@
 package puppy
 
+import (
+	"https://github.com/padmarajkore/dog"
+
+	"github.com/padmarajkore/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof! woof! woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
