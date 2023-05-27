b993d50c0c818a4fb92acf27056b5248b0458514
TEST <padmaraj.com@gmail.com>
2023-05-27 14:11:21 +0530

    latest

--- puppy.go
+++ puppy.go
@@ -1,8 +1,6 @@
 package puppy
 
 import (
-	"fmt"
-
 	"github.com/padmarajkore/dog"
 )
 
@@ -22,22 +20,22 @@ func BigBarks() string {
 	return dog.WhenGrownUp(Barks())
 }
 
-func From11() {
-	fmt.Println("I am version 1.1.0")
+func From11() string {
+	return ("I am version 1.1.0")
 }
 
-func From12() {
-	fmt.Println("I am version 1.2.0")
+func From12() string {
+	return ("I am version 1.2.0")
 }
 
-func From13() {
-	fmt.Println("I am version 1.3.0")
+func From13() string {
+	return ("I am version 1.3.0")
 }
 
-func Form14() {
-	fmt.Println("i am version 1.4.0")
+func Form14() string {
+	return ("i am version 1.4.0")
 }
 
-func Form15() {
-	fmt.Println("i am version 1.5.0")
+func Form15() string {
+	return ("i am version 1.5.0")
 }
